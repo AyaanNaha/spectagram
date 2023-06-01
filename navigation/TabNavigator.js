9961c837b439fe1097160bf77b3380eb424c51ca
AyaanNaha <ayaan.naha26@gmail.com>
2023-06-01 15:54:07 -0700

    Finished project 82

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,13 +1,15 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import Feed from "../screens/Feed";
 import CreatePost from "../screens/CreatePost";
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { RFValue } from "react-native-responsive-fontsize";
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const BottomTabNavigator = () => {
     return (
         <Tab.Navigator 
+            labeled={false}
             screenOptions={({route}) => ({
                 tabBarIcon: ({focused, color, size}) => {
                     let iconName;
@@ -16,7 +18,7 @@ const BottomTabNavigator = () => {
                     } else if (route.name === 'CreatePost') {
                         iconName = focused ? 'create' : 'create-outline';
                     }
-                    return <Ionicons name={iconName} size={size} color={color} />
+                    return <Ionicons name={iconName} size={RFValue(25)} color={color}/>
                 }
             })}
         >
